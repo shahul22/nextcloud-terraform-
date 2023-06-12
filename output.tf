67fbd4efaf1eb0d02c17c97ffaed316c32bbc49f
Shahul <shahulh71@gmail.com>
2023-06-12 15:43:41 +0300

    updated ignore file

--- output.tf
+++
@@ -1,3 +0,0 @@
-output "ecs_global_ip" {
-  value = alicloud_eip.ecs_eip[*].ip_address
-}
--- versions.tf
+++
@@ -1,5 +0,0 @@
-provider "alicloud" {
-  access_key = "${var.access_key}"
-  secret_key = "${var.secret_key}"
-  region     = "${var.region}"
-}
